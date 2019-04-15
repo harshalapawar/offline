e1f35d5ee1b336202623836698bf7508f10fbc8c
SUCHITRA SWAIN <suchitra.swain@auxesisgroup.com>
2019-04-15 19:13:05 +0530

    side bar logout

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { SessionStorageService } from "ngx-webstorage";
+import { Router } from '@angular/router';
 
 @Component({
   selector: "app-sidebar",
@@ -12,11 +13,16 @@ export class SidebarComponent implements OnInit {
   firstName: any;
   authorities: any;
   lastName: any;
-  constructor(private session: SessionStorageService) {}
+  constructor(private session: SessionStorageService, private _router: Router) { }
 
   ngOnInit() {
     this.firstName = this.session.retrieve("firstName");
     this.lastName = this.session.retrieve("lastName");
     this.authorities = this.session.retrieve("authorities");
+  }
+
+  logout() {
+    this._router.navigate(['']);
+    this.session.clear();
   }
 }
